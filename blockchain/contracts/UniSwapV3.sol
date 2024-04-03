46807853ebe0be6c769ed44a70cf8c7c56c64228
Ronnieraj37 <101713157+Ronnieraj37@users.noreply.github.com>
2024-04-03 10:22:51 +0530

    Added addToken functionality

--- blockchain/contracts/UniSwapV3.sol
+++ blockchain/contracts/UniSwapV3.sol
@@ -1,20 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/swap-router-contracts/contracts/interfaces/ISwapRouter02.sol";
 import "@uniswap/swap-router-contracts/contracts/interfaces/IV3SwapRouter.sol";
 
+error No_Amount_Given();
+error InputOutputSame(uint256 input);
+error OutOfBounds(uint256 input, uint256 output);
+error OnlyOwner();
+error TokenAlreadyExists();
+
 contract UniSwapV3 {
     ISwapRouter02 public swapRouter =
         ISwapRouter02(0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E);
 
-    address[] public erc20;
+    address owner;
+    uint256 public totalTokens;
+    mapping(uint => address) public tokenAddress;
+    mapping(address => bool) public tokenExists; // Prevent to add same token
+
     constructor() {
-        erc20 = [
-            0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14, //Wrapped Ether
-            0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984 //Uniswap
-        ];
+        owner = msg.sender;
+        addToken(0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
+        addToken(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
+    }
+
+    function addToken(address _tokenAddress) public {
+        if (msg.sender != owner) revert OnlyOwner();
+        if (tokenExists[_tokenAddress]) revert TokenAlreadyExists();
+
+        tokenAddress[totalTokens] = _tokenAddress;
+        tokenExists[_tokenAddress] = true;
+        totalTokens++;
     }
 
     function swapTokenInputSingle(
@@ -22,22 +40,27 @@ contract UniSwapV3 {
         uint256 input,
         uint256 output
     ) external returns (uint amountOut) {
-        require(amountIn >= 0, "Amount Cannot be 0");
-        require(input != output, "Input & output must be different");
-        require(input < erc20.length && output < erc20.length, "Out of Bounds");
+        if (amountIn == 0) revert No_Amount_Given();
+        if (input == output) revert InputOutputSame(input);
+        if (input >= totalTokens || output >= totalTokens)
+            revert OutOfBounds(input, output);
         TransferHelper.safeTransferFrom(
-            erc20[input],
+            tokenAddress[input],
             msg.sender,
             address(this),
             amountIn
         );
 
-        TransferHelper.safeApprove(erc20[input], address(swapRouter), amountIn);
+        TransferHelper.safeApprove(
+            tokenAddress[input],
+            address(swapRouter),
+            amountIn
+        );
 
         IV3SwapRouter.ExactInputSingleParams memory params = IV3SwapRouter
             .ExactInputSingleParams({
-                tokenIn: erc20[input],
-                tokenOut: erc20[output],
+                tokenIn: tokenAddress[input],
+                tokenOut: tokenAddress[output],
                 fee: 3000,
                 recipient: msg.sender,
                 amountIn: amountIn,
@@ -48,48 +71,48 @@ contract UniSwapV3 {
         amountOut = swapRouter.exactInputSingle(params);
     }
 
-    function swapTokenOutputSingle(
-        uint input,
-        uint output,
-        uint256 amountOut,
-        uint256 amountInMaximum
-    ) external returns (uint256 amountIn) {
-        require(amountInMaximum != 0);
-        require(input != output);
-        require(input < erc20.length && output < erc20.length, "Out of Bounds");
-        TransferHelper.safeTransferFrom(
-            erc20[input],
-            msg.sender,
-            address(this),
-            amountInMaximum
-        );
+    // function swapTokenOutputSingle(
+    //     uint256 input,
+    //     uint256 output,
+    //     uint256 amountOut,
+    //     uint256 amountInMaximum
+    // ) external returns (uint256 amountIn) {
+    //     require(amountInMaximum != 0);
+    //     require(input != output);
+    //     require(input < erc20.length && output < erc20.length, "Out of Bounds");
+    //     TransferHelper.safeTransferFrom(
+    //         erc20[input],
+    //         msg.sender,
+    //         address(this),
+    //         amountInMaximum
+    //     );
 
-        TransferHelper.safeApprove(
-            erc20[input],
-            address(swapRouter),
-            amountInMaximum
-        );
+    //     TransferHelper.safeApprove(
+    //         erc20[input],
+    //         address(swapRouter),
+    //         amountInMaximum
+    //     );
 
-        IV3SwapRouter.ExactOutputSingleParams memory params = IV3SwapRouter
-            .ExactOutputSingleParams({
-                tokenIn: erc20[input],
-                tokenOut: erc20[output],
-                fee: 3000,
-                recipient: msg.sender,
-                amountOut: amountOut,
-                amountInMaximum: amountInMaximum,
-                sqrtPriceLimitX96: 0
-            });
+    //     IV3SwapRouter.ExactOutputSingleParams memory params = IV3SwapRouter
+    //         .ExactOutputSingleParams({
+    //             tokenIn: erc20[input],
+    //             tokenOut: erc20[output],
+    //             fee: 3000,
+    //             recipient: msg.sender,
+    //             amountOut: amountOut,
+    //             amountInMaximum: amountInMaximum,
+    //             sqrtPriceLimitX96: 0
+    //         });
 
-        amountIn = swapRouter.exactOutputSingle(params);
+    //     amountIn = swapRouter.exactOutputSingle(params);
 
-        if (amountIn < amountInMaximum) {
-            TransferHelper.safeApprove(erc20[input], address(swapRouter), 0);
-            TransferHelper.safeTransfer(
-                erc20[input],
-                msg.sender,
-                amountInMaximum - amountIn
-            );
-        }
-    }
+    //     if (amountIn < amountInMaximum) {
+    //         TransferHelper.safeApprove(erc20[input], address(swapRouter), 0);
+    //         TransferHelper.safeTransfer(
+    //             erc20[input],
+    //             msg.sender,
+    //             amountInMaximum - amountIn
+    //         );
+    //     }
+    // }
 }
--- blockchain/test/UniswapTest.js
+++ blockchain/test/UniswapTest.js
@@ -3,23 +3,20 @@ const { ethers } = require('hardhat');
 describe("UniswapTest", function () {
 
   describe("Deployment", function () {
-    let uniswapContract;
+
+    const Uniswap = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+    const WETH9 = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14";
+
+    let tokenExchangeContract;
     let accounts;
-
-    let Uniswap = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
-    let WETH9 = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14";
-
     let UniswapTokenContract;
     let WETH9Contract;
-    let tokenPrivate;
-    let tokenPrivateAddress;
+
     before(async () => {
       accounts = await ethers.getSigners();
       const UniswapV3 = await ethers.getContractFactory('UniSwapV3');
-      uniswapContract = await UniswapV3.deploy();
-      await uniswapContract.deployed();
-      tokenPrivateAddress = await uniswapContract.gettokenPrivate();
-      tokenPrivate = await ethers.getContractAt('MyToken', tokenPrivateAddress);
+      tokenExchangeContract = await UniswapV3.deploy();
+      await tokenExchangeContract.deployed();
       UniswapTokenContract = await ethers.getContractAt('IERC20', Uniswap);
       WETH9Contract = await ethers.getContractAt('IWETH', WETH9);
     })
@@ -28,56 +25,36 @@ describe("UniswapTest", function () {
       expect(UniswapTokenContract).not.equal(WETH9Contract);
     });
 
+    it("Check Error for NoAmountGiven", async function () {
+      await expect(tokenExchangeContract.swapTokenInputSingle(0, 0, 1)).to.be.revertedWithCustomError(tokenExchangeContract, "No_Amount_Given");
+    })
+
+    it("Check Error for InputOutputSame", async function () {
+      await expect(tokenExchangeContract.swapTokenInputSingle(10000, 0, 0)).to.be.revertedWithCustomError(tokenExchangeContract, "InputOutputSame");
+    })
+
+    it("Check Error for OutOfBounds", async function () {
+      await expect(tokenExchangeContract.swapTokenInputSingle(10000, 2, 1)).to.be.revertedWithCustomError(tokenExchangeContract, "OutOfBounds");
+    })
+
+    it("Check for adding exisitng token", async function () {
+      await expect(tokenExchangeContract.addToken("0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14")).to.be.revertedWithCustomError(tokenExchangeContract, "TokenAlreadyExists");
+    })
+
     it("Checking for Swap in WETH/uniswap", async function () {
 
       const amount = 10n ** 18n;
       await WETH9Contract.deposit({ value: amount });
-      await WETH9Contract.approve(uniswapContract.address, amount);
-      await uniswapContract.swapTokenInputSingle(1000000, 0, 1);
-    })
-
-    it("Adding Liquidity for uniswap/WETH", async = async () => {
-
-      const amount = 10n ** 18n;
-      const MintParams = {
-        token0: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
-        token1: '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14',
-        fee: 3000,
-        tickLower: -1000,
-        tickUpper: 1000,
-        amount0Desired: amount,
-        amount1Desired: amount,
-        amount0Min: 10,
-        amount1Min: 10,
-        recipient: accounts[0],
-        deadline: 1785767499,
-      }
-
-      await WETH9Contract.approve(uniswapContract.address, amount);
-      await UniswapTokenContract.approve(uniswapContract.address, amount);
-      const res = await uniswapContract.increaseLiquidity(MintParams);
-      // console.log("Swap",res);
-      // await tokenPrivate.mint(accounts[0].address, amount);
+      await WETH9Contract.approve(tokenExchangeContract.address, amount);
+      await tokenExchangeContract.swapTokenInputSingle(amount, 0, 1);
     })
 
     // it("Checking for Swap in WETH/uniswap using fixed Output", async function () {
-
     //   const amountInMax = 10n ** 18n;
     //   const uniswapOut = 100n * 10n ** 18n;
     //   await WETH9Contract.deposit({ value: amountInMax });
-    //   await WETH9Contract.approve(uniswapContract.address, amountInMax);
-    //   await uniswapContract.swapTokenOutputSingle(0, 1, uniswapOut, amountInMax);
-
-    // })
-
-    // it("Checking for Swap in uniswap/WETH using fixed Output", async function () {
-
-    //   const amountInMax = 100n * 10n ** 18n;
-    //   const wethOut = 10n ** 18n;
-    //   await UniswapContract.mint(accounts[0].address, amount);
-    //   await UniswapContract.approve(uniswapContract.address, amountInMax);
-    //   await uniswapContract.swapTokenOutputSingle(1, 0, wethOut, amountInMax);
-
+    //   await WETH9Contract.approve(tokenExchangeContract.address, amountInMax);
+    //   await tokenExchangeContract.swapTokenOutputSingle(0, 1, uniswapOut, amountInMax);
     // })
 
   });
